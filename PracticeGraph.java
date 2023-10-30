b5f6371dec209ccc79777e59f55f4243da71b97c
Henry Ji <jeep.jipu@gmail.com>
2023-10-30 13:55:57 +0800

    添加测试用加权有向图

--- PracticeGraph.java
+++ PracticeGraph.java
@@ -800,6 +800,26 @@ class WeightedDigraph {
         System.out.println(g.edges());
     }
 
+    public static WeightedDigraph get_test_graph() {
+        WeightedDigraph g = new WeightedDigraph(8);
+        g.addEdge(new DirectedEdge(4, 5, 0.35));
+        g.addEdge(new DirectedEdge(5, 4, 0.35));
+        g.addEdge(new DirectedEdge(4, 7, 0.37));
+        g.addEdge(new DirectedEdge(5, 7, 0.28));
+        g.addEdge(new DirectedEdge(7, 5, 0.28));
+        g.addEdge(new DirectedEdge(5, 1, 0.32));
+        g.addEdge(new DirectedEdge(0, 4, 0.38));
+        g.addEdge(new DirectedEdge(0, 2, 0.26));
+        g.addEdge(new DirectedEdge(7, 3, 0.39));
+        g.addEdge(new DirectedEdge(1, 3, 0.29));
+        g.addEdge(new DirectedEdge(2, 7, 0.34));
+        g.addEdge(new DirectedEdge(6, 2, 0.40));
+        g.addEdge(new DirectedEdge(3, 6, 0.52));
+        g.addEdge(new DirectedEdge(6, 0, 0.58));
+        g.addEdge(new DirectedEdge(6, 4, 0.93));
+        return g;
+    }
+
     private final int V;
     private int E;
     private Queue<DirectedEdge>[] adj;
