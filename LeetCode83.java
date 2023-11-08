732db3d406f2eb34da3e003da450a64a6ee22999
Henry Ji <jeep.jipu@gmail.com>
2023-11-08 20:11:39 +0800

    add LeetCode83

---
+++ LeetCode83.java
@@ -0,0 +1,57 @@
+// https://leetcode.cn/problems/remove-duplicates-from-sorted-list/description/
+
+public class LeetCode83 {
+    public static void main(String[] args) {
+
+        ListNode head1 = new ListNode(1, null);
+        head1.next = new ListNode(1, null);
+        head1.next.next = new ListNode(2, null);
+        System.out.println(head1);
+        new Solution().deleteDuplicates(head1);
+        System.out.println(head1);
+        System.out.println("================================================");
+
+        ListNode head2 = new ListNode(1, null);
+        head2.next = new ListNode(1, null);
+        head2.next.next = new ListNode(2, null);
+        head2.next.next.next = new ListNode(3, null);
+        head2.next.next.next.next = new ListNode(3, null);
+        System.out.println(head2);
+        new Solution().deleteDuplicates(head2);
+        System.out.println(head2);
+        System.out.println("================================================");
+
+        ListNode head3 = new ListNode();
+        System.out.println(head3);
+        new Solution().deleteDuplicates(head3);
+        System.out.println(head3);
+        System.out.println("================================================");
+
+        ListNode head4 = new ListNode(1, null);
+        head4.next = new ListNode(1, null);
+        head4.next.next = new ListNode(1, null);
+        System.out.println(head4);
+        new Solution().deleteDuplicates(head4);
+        System.out.println(head4);
+        System.out.println("================================================");
+    }
+}
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode node = head;
+        while (node != null && node.next != null) {
+            if (node.next.val == node.val) {
+                if (node.next.next != null) {
+                    node.next = node.next.next;
+                } else {
+                    node.next = null;
+                    break;
+                }
+            } else {
+                node = node.next;
+            }
+        }
+        return head;
+    }
+}
