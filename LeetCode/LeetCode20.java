b7dc9ca98f45c6ef73693eaa3ee25ae585882729
Henry Ji <jeep.jipu@gmail.com>
2023-11-12 12:06:27 +0800

    add LeetCode20

---
+++ LeetCode/LeetCode20.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+import java.util.HashMap;
+
+public class LeetCode20 {
+    public static void main(String[] args) {
+        System.out.println(new Solution().isValid("()"));
+        System.out.println(new Solution().isValid("()[]{}"));
+        System.out.println(new Solution().isValid("(]"));
+    }
+}
+
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 == 1) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        HashMap<Character, Character> dict = new HashMap<>();
+        dict.put(')', '(');
+        dict.put(']', '[');
+        dict.put('}', '{');
+
+        for (int i = 0; i < s.length(); i++) {
+            Character ch = s.charAt(i);
+            if (dict.containsKey(ch)) {
+                if (stack.isEmpty() || stack.peek() != dict.get(ch)) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
