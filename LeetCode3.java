feec2d5986447b22dd2cf62d652f30b278c6c348
Henry Ji <jeep.jipu@gmail.com>
2023-11-12 09:40:02 +0800

    add LeetCode3

---
+++ LeetCode3.java
@@ -0,0 +1,32 @@
+// https://leetcode.cn/problems/longest-substring-without-repeating-characters/description/
+
+// 思路: 滑动窗口
+
+import java.util.HashSet;
+
+public class LeetCode3 {
+    public static void main(String[] args) {
+        System.out.println(new Solution().lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(new Solution().lengthOfLongestSubstring("bbbbb"));
+        System.out.println(new Solution().lengthOfLongestSubstring("pwwkew"));
+    }
+}
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int ans = 0;
+        int rk = -1;
+        HashSet<Character> set = new HashSet<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            if (i != 0) {
+                set.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < s.length() && !set.contains(s.charAt(rk + 1))) {
+                set.add(s.charAt(rk + 1));
+                rk++;
+            }
+            ans = Math.max(ans, set.size());
+        }
+        return ans;
+    }
+}
