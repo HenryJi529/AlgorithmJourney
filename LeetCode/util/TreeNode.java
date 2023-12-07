8ca60eaab55287bb8876101f54fa527193714094
Henry Ji <jeep.jipu@gmail.com>
2023-12-07 20:31:19 +0800

    实现TreeNode toString方法

--- LeetCode/util/TreeNode.java
+++ LeetCode/util/TreeNode.java
@@ -1,5 +1,9 @@
 package util;
 
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
@@ -16,5 +20,24 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public String toString() {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        TreeNode node;
+        queue.offer(this);
+        while (!queue.isEmpty()) {
+            node = queue.poll();
+
+            if (node != null) {
+                list.add(node.val);
+                queue.offer(node.left);
+                queue.offer(node.right);
+            } else {
+                list.add(null);
+            }
+        }
+        return list.toString();
     }
 }
