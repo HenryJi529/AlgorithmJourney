ea1b68428cb9df367526ebd60e14192593a79285
Henry Ji <jeep.jipu@gmail.com>
2023-12-22 21:22:13 +0800

    实现Tree的辅助构建函数

--- LeetCode/util/TreeNode.java
+++ LeetCode/util/TreeNode.java
@@ -40,4 +40,46 @@ public class TreeNode {
         }
         return list.toString();
     }
+
+    public static TreeNode buildTree(Integer[] nodes) {
+        if (nodes.length == 0 || nodes[0] == null) {
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+        TreeNode root = new TreeNode(nodes[0]);
+        queue.add(root);
+        int i = 1;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (i == nodes.length) {
+                // 已经没有多余的节点了
+                break;
+            } else if (i == nodes.length - 1) {
+                // 还剩下一个左节点
+                if (nodes[i] != null) {
+                    node.left = new TreeNode(nodes[i]);
+                    queue.offer(node.left);
+                }
+                break;
+            } else {
+                if (nodes[i] != null) {
+                    node.left = new TreeNode(nodes[i]);
+                    queue.offer(node.left);
+                }
+                if (nodes[i + 1] != null) {
+                    node.right = new TreeNode(nodes[i + 1]);
+                    queue.offer(node.right);
+                }
+                i += 2;
+            }
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = TreeNode.buildTree(new Integer[] { 3, 9, 20, null, null, 15, 7 });
+        System.out.println(root);
+    }
 }
