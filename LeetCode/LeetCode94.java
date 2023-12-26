a7e17cb6be8d36b1c862b7f55dfd28dd0ad93d7f
Henry Ji <jeep.jipu@gmail.com>
2023-12-26 22:39:18 +0800

    LeetCode94: 添加迭代版本

--- LeetCode/LeetCode94.java
+++ LeetCode/LeetCode94.java
@@ -1,22 +1,25 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Deque;
 
 import util.TreeNode;
 
 public class LeetCode94 {
     public static void main(String[] args) {
-        System.out.println(new Solution94().inorderTraversal(TreeNode.buildTree(new Integer[] { 1, null, 2, 3 })));
+        System.out.println(new Solution94_2().inorderTraversal(TreeNode.buildTree(new Integer[] { 1, null, 2, 3 })));
         System.out.println("================================================");
 
-        System.out.println(new Solution94().inorderTraversal(TreeNode.buildTree(new Integer[] {})));
+        System.out.println(new Solution94_2().inorderTraversal(TreeNode.buildTree(new Integer[] {})));
         System.out.println("================================================");
 
-        System.out.println(new Solution94().inorderTraversal(TreeNode.buildTree(new Integer[] { 1 })));
+        System.out.println(new Solution94_2().inorderTraversal(TreeNode.buildTree(new Integer[] { 1 })));
         System.out.println("================================================");
     }
 }
 
-class Solution94 {
+class Solution94_1 {
+    // NOTE: 迭代版本
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> numbers = new ArrayList<Integer>();
         inorderTraversal(root, numbers);
@@ -30,5 +33,45 @@ class Solution94 {
         inorderTraversal(x.left, numbers);
         numbers.add(x.val);
         inorderTraversal(x.right, numbers);
+    }
+}
+
+class Solution94_2 {
+    // NOTE: 迭代版本
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        TreeNode pre = null;
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.peek();
+            if (pre == null || pre.left == cur || pre.right == cur) {
+                if (cur.left != null) {
+                    stack.push(cur.left);
+                } else {
+                    res.add(cur.val);
+                    if (cur.right != null) {
+                        stack.push(cur.right);
+                    } else {
+                        stack.pop();
+                    }
+                }
+            } else if (cur.right == pre) {
+                stack.pop();
+            } else if (cur.left == pre) {
+                res.add(cur.val);
+                if (cur.right != null) {
+                    stack.push(cur.right);
+                } else {
+                    stack.pop();
+                }
+            }
+            pre = cur;
+        }
+
+        return res;
     }
 }
