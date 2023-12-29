9e5e3370d6c1063fe4f30fff4005429bdaa8ff9e
Henry Ji <jeep.jipu@gmail.com>
2023-12-29 09:45:01 +0800

    LeetCode181: 添加join版本解法

--- LeetCode/LeetCode181.sql
+++ LeetCode/LeetCode181.sql
@@ -3,3 +3,7 @@ from Employee as a, Employee as b
 where
     a.ManagerId = b.id
     AND a.Salary > b.Salary
+
+SELECT a.NAME AS Employee
+FROM Employee AS a
+    JOIN Employee AS b ON a.ManagerId = b.Id AND a.Salary > b.Salary;
