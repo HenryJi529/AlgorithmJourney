0a6fdcd9cf7a6149b1adbb92ea4f86ff82738542
Henry Ji <jeep.jipu@gmail.com>
2024-01-02 10:26:44 +0800

    整理LeetCode150

--- LeetCode/LeetCode150.java
+++ LeetCode/LeetCode150.java
@@ -1,23 +1,19 @@
-/* 
- * 问题描述: https://leetcode.cn/problems/evaluate-reverse-polish-notation/description/
- */
-
 import java.util.Stack;
 
 public class LeetCode150 {
     public static void main(String[] args) {
         String[] tokens1 = new String[] { "2", "1", "+", "3", "*" };
-        System.out.println(new Solution().evalRPN(tokens1));
+        System.out.println(new Solution150().evalRPN(tokens1));
 
         String[] tokens2 = new String[] { "4", "13", "5", "/", "+" };
-        System.out.println(new Solution().evalRPN(tokens2));
+        System.out.println(new Solution150().evalRPN(tokens2));
 
         String[] tokens3 = new String[] { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" };
-        System.out.println(new Solution().evalRPN(tokens3));
+        System.out.println(new Solution150().evalRPN(tokens3));
     }
 }
 
-class Solution {
+class Solution150 {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<Integer>();
         for (int i = 0; i < tokens.length; i++) {
