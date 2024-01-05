07129c470c752483ebec8a038a20aac3aa661c84
Henry Ji <jeep.jipu@gmail.com>
2024-01-05 21:13:35 +0800

    LeetCode23: 简化测试代码

--- LeetCode/LeetCode23.java
+++ LeetCode/LeetCode23.java
@@ -1,22 +1,15 @@
 import java.util.PriorityQueue;
 import java.util.Comparator;
+
 import util.ListNode;
 
 public class LeetCode23 {
     public static void main(String[] args) {
         ListNode[] list;
-        ListNode n1, n2, n3;
         // 输入：lists = [[1,4,5],[1,3,4],[2,6]]
         // 输出：[1,1,2,3,4,4,5,6]
-        n1 = new ListNode(1);
-        n1.next = new ListNode(4);
-        n1.next.next = new ListNode(5);
-        n2 = new ListNode(1);
-        n2.next = new ListNode(3);
-        n2.next.next = new ListNode(4);
-        n3 = new ListNode(2);
-        n3.next = new ListNode(6);
-        list = new ListNode[] { n1, n2, n3 };
+        list = new ListNode[] { ListNode.buildLinkedList(new Integer[] { 1, 4, 5 }),
+                ListNode.buildLinkedList(new Integer[] { 1, 3, 4 }), ListNode.buildLinkedList(new Integer[] { 2, 6 }) };
         System.out.println(new Solution23().mergeKLists(list));
 
         // 输入：lists = []
@@ -26,8 +19,7 @@ public class LeetCode23 {
 
         // 输入：lists = [[]]
         // 输出：[]
-        n1 = null;
-        list = new ListNode[] { n1 };
+        list = new ListNode[] { null };
         System.out.println(new Solution23().mergeKLists(list));
     }
 }
@@ -50,12 +42,6 @@ class Solution23 {
         }
         ListNode currentNode = result;
         while (queue.size() > 0) {
-            // ListNode minListNode = queue.poll();
-            // if (minListNode.next != null) {
-            // queue.add(minListNode.next);
-            // }
-            // currentNode.next = minListNode;
-
             currentNode.next = queue.poll();
             if (currentNode.next.next != null) {
                 queue.add(currentNode.next.next);
