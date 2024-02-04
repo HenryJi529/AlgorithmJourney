74c8a83aa2365451733dbf6fef74b26beb16571f
Henry Ji <jeep.jipu@gmail.com>
2024-02-04 15:21:59 +0800

    update ExperimentStringHashCode

--- BasicPractice/ExperimentStringHashCode.java
+++ BasicPractice/ExperimentStringHashCode.java
@@ -3,10 +3,12 @@ public class ExperimentStringHashCode {
         // 创建两个相同内容的String
         String str1 = "1234";
         String str2 = "12345".substring(0, 4);
+        String str3 = "1234";
 
-        // 检查数组内容是否相同
-        boolean contentEquals = str1.equals(str2);
-        System.out.println("Content equals: " + contentEquals);
+        // 检查数组是否相同
+        System.out.println("str1 str2 Content equals: " + str1.equals(str2));
+        System.out.println("str1 str3 Reference equals: " + (str1 == str2));
+        System.out.println("str1 str3 Reference equals: " + (str1 == str3));
 
         // 检查 hashCode 是否相同
         boolean hashCodeEqualsNaive = (str1.hashCode() == str2.hashCode());
