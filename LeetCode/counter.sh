464e06cca963c745cfad149a26322f04e9bb6a84
Henry Ji <jeep.jipu@gmail.com>
2024-02-14 15:58:09 +0800

    update counter.sh

--- LeetCode/counter.sh
+++ LeetCode/counter.sh
@@ -1,10 +1,8 @@
 # 只统计有效题目
 javaNum=$(ls -l *.java | wc -l)
-pythonNum=$(ls -l *.py | wc -l)
 shellNum=$(ls -l shell/*.sh | wc -l)
 sqlNum=$(ls -l sql/*.sql | wc -l)
-echo "javaNum=$javaNum"
-echo "pythonNum=$pythonNum"
-echo "shellNum=$shellNum"
-echo "sqlNum=$sqlNum"
-echo $(($javaNum + $pythonNum + $shellNum + $sqlNum))
+echo "javaNum = $javaNum"
+echo "shellNum = $shellNum"
+echo "sqlNum = $sqlNum"
+echo "totalNum = $(($javaNum + $shellNum + $sqlNum))"
