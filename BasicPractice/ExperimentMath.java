519e3247b404e0a20867b8b30449eb3f6da8f467
Henry Ji <jeep.jipu@gmail.com>
2024-03-13 09:52:51 +0800

    add ExperimentMath.java

---
+++ BasicPractice/ExperimentMath.java
@@ -0,0 +1,5 @@
+public class ExperimentMath {
+    public static void main(String[] args) {
+        System.out.println("round(11.5) + round(-11.5) = " + (Math.round(-11.5) + Math.round(11.5)));
+    }
+}
