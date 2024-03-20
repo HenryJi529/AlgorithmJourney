fb7eb2695d1fc220f8ed8be5dfefc2b948b80938
Henry Ji <jeep.jipu@gmail.com>
2024-03-20 21:21:52 +0800

    add ExperimentIdentifier

---
+++ BasicPractice/ExperimentIdentifier.java
@@ -0,0 +1,9 @@
+public class ExperimentIdentifier {
+    public static void main(String[] args) {
+        boolean 是男 = false;
+        System.out.println(是男);
+
+        boolean _isMale = false;
+        System.out.println(_isMale);
+    }
+}
