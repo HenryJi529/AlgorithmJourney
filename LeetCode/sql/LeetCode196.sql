205a3953f9bb722e57b4bf02be809dacdfda0ecf
Henry Ji <jeep.jipu@gmail.com>
2024-04-17 22:24:10 +0800

    LeetCode196: 简化代码

--- LeetCode/sql/LeetCode196.sql
+++ LeetCode/sql/LeetCode196.sql
@@ -1,11 +1,8 @@
 delete from Person
-where id not in (
-        select id
-        from (
-                select
-                    MIN(id) as id
-                from Person
-                group by
-                    Email
-            ) t
+where
+    id not in(
+        select MIN(id) as id
+        from Person
+        group by
+            Email
     );
