78dab761b66bf8cd973dc8f4979106b1efaba576
Henry Ji <jeep.jipu@gmail.com>
2024-07-09 22:33:43 +0800

    update ExperimentIntArrayHashCode.java

--- BasicPractice/ExperimentIntArrayHashCode.java
+++ BasicPractice/ExperimentIntArrayHashCode.java
@@ -1,17 +1,12 @@
 import java.util.Arrays;
-import java.util.HashSet;
 
 public class ExperimentIntArrayHashCode {
     public static void main(String[] args) {
         // 创建两个相同内容的 int[] 数组
         int[] array1 = new int[] { 1, 2, 3 };
         int[] array2 = Arrays.copyOf(array1, array1.length);
-
-        // 输出数组的内容和哈希码
         System.out.println("Array 1: " + Arrays.toString(array1));
         System.out.println("Array 2: " + Arrays.toString(array2));
-        System.out.println("HashCode 1: " + Arrays.hashCode(array1));
-        System.out.println("HashCode 2: " + Arrays.hashCode(array2));
 
         // 检查数组内容是否相同
         boolean contentEquals = Arrays.equals(array1, array2);
@@ -22,13 +17,5 @@ public class ExperimentIntArrayHashCode {
         System.out.println("ArrayHashCode equals: " + hashCodeEqualsArray);
         boolean hashCodeEqualsNaive = (array1.hashCode() == array2.hashCode());
         System.out.println("NaiveHashCode equals: " + hashCodeEqualsNaive);
-
-        HashSet<int[]> set = new HashSet<int[]>();
-        set.add(array1);
-        if (set.contains(array2)) {
-            System.out.println("HashSet按照int[]内容计算");
-        } else {
-            System.out.println("HashSet按照int[]地址计算");
-        }
     }
 }
