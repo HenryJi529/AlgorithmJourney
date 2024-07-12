e517be8205ff14f0ec960695ccae73227d9544dc
Henry Ji <jeep.jipu@gmail.com>
2024-07-12 22:27:11 +0800

    add LeetCode416

---
+++ LeetCode/LeetCode416.java
@@ -0,0 +1,82 @@
+public class LeetCode416 {
+    public static void main(String[] args) {
+        // 输入：nums = [1,5,11,5]
+        // 输出：true
+        System.out.println(new Solution416_2().canPartition(new int[] { 1, 5, 11, 5 }));
+
+        // 输入：nums = [1,2,3,5]
+        // 输出：false
+        System.out.println(new Solution416_2().canPartition(new int[] { 1, 2, 3, 5 }));
+    }
+}
+
+/**
+ * 针对NP问题，采用时间复杂度与元素大小相关的动态规划
+ */
+class Solution416_1 {
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return false;
+        }
+        int sum = 0, maxNum = 0;
+        for (int num : nums) {
+            sum += num;
+            maxNum = Math.max(maxNum, num);
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int target = sum / 2;
+        if (maxNum > target) {
+            return false;
+        }
+        boolean[][] dp = new boolean[n][target + 1];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        dp[0][nums[0]] = true;
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+            for (int j = 1; j <= target; j++) {
+                if (j >= num) {
+                    dp[i][j] = dp[i - 1][j] | dp[i - 1][j - num];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n - 1][target];
+    }
+}
+
+class Solution416_2 {
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return false;
+        }
+        int sum = 0, maxNum = 0;
+        for (int num : nums) {
+            sum += num;
+            maxNum = Math.max(maxNum, num);
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int target = sum / 2;
+        if (maxNum > target) {
+            return false;
+        }
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            // NOTE: 这里需要倒序，因为如果正序一轮中考前的会影响靠后的
+            for (int j = target; j >= num; --j) {
+                dp[j] |= dp[j - num];
+            }
+        }
+        return dp[target];
+    }
+}
