391511e89e65e1a565760c2da8f2066846b834c7
Henry Ji <jeep.jipu@gmail.com>
2024-07-21 16:55:54 +0800

    update LeetCode607

--- LeetCode/sql/LeetCode607.sql
+++ LeetCode/sql/LeetCode607.sql
@@ -1,10 +1,32 @@
+-- 嵌套查询
 select name
 from SalesPerson
 where
     sales_id not in(
-        select Orders.sales_id
+        select sales_id
         from Orders
-            left join Company on Company.com_id = Orders.com_id
         where
-            name = "RED"
+            com_id = (
+                select com_id
+                from Company
+                where
+                    name = 'RED'
+            )
     );
+
+-- group by(需要注意公司名不存在的情况)
+select p.name
+from SalesPerson p
+    left join Orders o on p.sales_id = o.sales_id
+group by
+    p.sales_id
+having
+    SUM(
+        (o.com_id is not null)
+        and o.com_id in (
+            select com_id
+            from Company
+            where
+                name = 'RED'
+        )
+    ) = 0;
