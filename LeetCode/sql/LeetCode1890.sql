3cf954e4801ed08afb0a8d09408d3c5484b2bae3
Henry Ji <jeep.jipu@gmail.com>
2024-07-21 22:14:08 +0800

    update LeetCode1890

--- LeetCode/sql/LeetCode1890.sql
+++ LeetCode/sql/LeetCode1890.sql
@@ -1,6 +1,22 @@
+-- 直接了当选择time_stamp最大值
 select user_id, MAX(time_stamp) last_stamp
 from Logins
 where
     year(time_stamp) = 2020
 group by
     user_id;
+
+-- 通过row_number()排序
+select user_id, time_stamp last_stamp
+from (
+        select user_id, time_stamp, row_number() over (
+                partition by
+                    user_id
+                order by time_stamp desc
+            ) ranking
+        from Logins
+        where
+            year(time_stamp) = 2020
+    ) t
+where
+    ranking = 1;
