9bcf495fb1ab5b8789a1ea74d28dd7c12a57b853
Henry Ji <jeep.jipu@gmail.com>
2024-07-21 15:55:42 +0800

    update LeetCode184

--- LeetCode/sql/LeetCode184.sql
+++ LeetCode/sql/LeetCode184.sql
@@ -1,3 +1,4 @@
+-- 先查每个dpeartment中e.salary最大值，后寻找最大值对应的e.name
 select d.name 'Department', e.name 'Employee', e.salary 'Salary'
 from Employee e
     left join Department d on d.id = e.departmentId
@@ -13,3 +14,18 @@ where
         where
             e.departmentId = t.departmentId
     );
+
+-- 用窗口函数进行分组排序
+select Department, Employee, Salary
+from (
+        select
+            d.name Department, e.name Employee, e.salary Salary, dense_rank() over (
+                partition by
+                    d.id
+                order by e.salary desc
+            ) ranking
+        from Employee e
+            join Department d on e.departmentId = d.id
+    ) t
+where
+    ranking = 1;
