ce5d4f8861c4ca5c981b18a9db13b56a16f59f79
Henry Ji <jeep.jipu@gmail.com>
2024-07-21 15:05:38 +0800

    update LeetCode183

--- LeetCode/sql/LeetCode183.sql
+++ LeetCode/sql/LeetCode183.sql
@@ -1,7 +1,16 @@
+-- 通过子查询
 select name as Customers
 from Customers
-where Customers.id not in (
-        select
-            DISTINCT customerid
+where
+    Customers.id not in(
+        select DISTINCT
+            customerid
         from Orders
     );
+
+-- 通过连接
+select name Customers
+from customers
+    left join orders on customers.id = orders.customerId
+where
+    customerId is null;
