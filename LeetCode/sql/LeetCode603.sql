9a87dce333594b26792ac25a132a732a1752b7a0
Henry Ji <jeep.jipu@gmail.com>
2024-07-22 20:17:08 +0800

    update LeetCode603

--- LeetCode/sql/LeetCode603.sql
+++ LeetCode/sql/LeetCode603.sql
@@ -1,10 +1,25 @@
+-- 连接
 select distinct
     a.seat_id
 from Cinema a
-    cross join Cinema b on a.free = 1
+    join Cinema b on a.free = 1
     and b.free = 1
-    and (
-        a.seat_id = b.seat_id + 1
-        or a.seat_id = b.seat_id - 1
-    )
+    and abs(a.seat_id - b.seat_id) = 1
 order by a.seat_id;
+
+-- 窗口函数
+select seat_id
+from (
+        select seat_id, lead(free, 1, 0) over (
+                order by seat_id
+            ) 'pre_free', free 'curr_free', lag(free, 1, 0) over (
+                order by seat_id
+            ) 'next_free'
+        from Cinema
+    ) t
+where
+    curr_free
+    and (
+        pre_free
+        or next_free
+    );
