ad74fe8f5ac99c197c73e96e486640fc27a84e13
Henry Ji <jeep.jipu@gmail.com>
2024-07-23 08:12:51 +0800

    update LeetCode534

--- LeetCode/sql/LeetCode534.sql
+++ LeetCode/sql/LeetCode534.sql
@@ -1,3 +1,4 @@
+-- 自连接
 select a.player_id, a.event_date, SUM(b.games_played) games_played_so_far
 from Activity a
     inner join Activity b on a.player_id = b.player_id
@@ -5,3 +6,16 @@ from Activity a
 group by
     a.player_id,
     a.event_date;
+
+-- select中做查询
+select a1.player_id, a1.event_date, (
+        select SUM(a2.games_played)
+        from Activity a2
+        where
+            a1.player_id = a2.player_id
+            and a1.event_date >= a2.event_date
+    ) games_played_so_far
+from Activity a1
+group by
+    a1.player_id,
+    a1.event_date;
